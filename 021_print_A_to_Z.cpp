c5adfcbe31cd20cfb1f81120017e0b7ae6aeca85
raihanbiswas2006 <raihanbiswas2006@gmail.com>
2025-08-20 23:54:24 +0600

    Print A to Z

---
+++ 021_print_A_to_Z.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ 
+    char alphabet;
+
+    for (alphabet = 'A'; alphabet <= 'Z'; alphabet++) {
+        
+        cout << alphabet << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
