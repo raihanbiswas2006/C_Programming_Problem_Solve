57c4f4d704a955fe8d843e73b722a325f7afe551
raihanbiswas2006 <raihanbiswas2006@gmail.com>
2025-08-22 00:23:04 +0600

    LCM Two Numbers

---
+++ 027_LCM_of_two_numbers.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int lcm, a, b, i;
+
+    cout << "Give Two Positive Numbers: ";
+    cin >> a >> b;
+
+    for (i = 1; i <= a && i <= b; i++)
+    {
+        if (a % i == 0 && b % i == 0)
+        {
+            lcm = i;
+        }
+    }
+
+    cout << "LCM of " <<a<< " and " <<b<< " is: " << lcm <<endl;
+
+    return 0;
+}
