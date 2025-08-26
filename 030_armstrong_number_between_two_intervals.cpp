307f9c4840d65a68a66e8204feb0bfc53225b2b3
raihanbiswas2006 <raihanbiswas2006@gmail.com>
2025-08-26 07:16:05 +0600

    Armstrong Number between Two Intervals

---
+++ 030_armstrong_number_between_two_intervals.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main(){
+
+    int lowerBound, upperBound;
+
+    cout << "Give the lower bound: ";
+    cin >> lowerBound;
+    cout << "Give the upper bound: ";
+    cin >> upperBound;
+
+    for (int i = lowerBound; i <= upperBound; i++)
+    {
+        int n, newNumber = 0;
+
+        n = i;
+
+        string strNumber = to_string(n);
+        int numDigits = strNumber.length();
+
+        int a[numDigits];
+
+        for (int i = 0; i < numDigits; i++)
+        {
+            a[i] = strNumber[i] - '0';
+        }
+
+        for (int i = 0; i < numDigits; i++)
+        {
+            newNumber += pow(a[i], numDigits);
+        }
+
+        if (newNumber == n)
+        {
+            cout << n << " ";
+        }
+    }
+
+    return 0;
+}
