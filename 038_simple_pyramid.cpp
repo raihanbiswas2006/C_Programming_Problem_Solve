790b0b5690dde94517bbff213a9d23934772ee0f
raihanbiswas2006 <raihanbiswas2006@gmail.com>
2025-08-27 01:44:42 +0600

    Simple Pyramid

---
+++ 038_simple_pyramid.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int row, i, j;
+
+    cout << "Give Number of Rows: ";
+    cin >> row;
+
+    for (i = 1; i <= row; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
