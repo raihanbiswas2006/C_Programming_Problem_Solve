8b77ac8eff37e990182b7d6bd68ba1a4859c4080
raihanbiswas2006 <raihanbiswas2006@gmail.com>
2025-08-27 01:57:19 +0600

    Int to Char Conversion

---
+++ 111_int_to_char_conversion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int num;
+    char ch;
+
+    cout << "Give an Int: ";
+    cin >> num;
+
+    ch = num;
+
+    cout << "The Char is: " << ch <<endl;
+
+    return 0;
+}
