0f0335f9f64d6d30d0e52f9f3af78e25153dd39e
Raihan Biswas <raihanbiswas2006@gmail.com>
2025-11-07 23:01:52 +0600

    Traingle

---
+++ 039_given_traingle.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int n, inpuT;
+
+    cout << "Give Number of Rows: ";
+    cin >> n;
+    cout << "Give Any Number: ";
+    cin >> inpuT;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int space = 1; space <= n - i; space++)
+        {
+            cout << " ";
+        }
+        for (int star = 1; star <= (2 * i - 1); star++)
+        {
+            cout << inpuT;
+        }
+        cout <<endl;
+    }
+
+    return 0;
+}
