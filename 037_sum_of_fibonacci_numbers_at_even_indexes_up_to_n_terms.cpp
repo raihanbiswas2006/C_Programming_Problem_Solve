519e2be426e65d9e7be7d772fcd5d95585d47a2b
Raihan Biswas <raihanbiswas2006@gmail.com>
2025-11-20 04:50:09 +0600

    Sum of Fibonacci Numbers at Even Indexes up to N Terms

---
+++ 037_sum_of_fibonacci_numbers_at_even_indexes_up_to_n_terms.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+
+    cout << "Give Number of Terms: ";
+    cin >> n;
+
+    long long first = 0, second = 1, next;
+    long long sum = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (i == 1)
+            next = first;
+        else if (i == 2)
+            next = second;
+        else
+        {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+
+        if (i % 2 == 0)
+            sum += next;
+    }
+
+    cout << "Sum of Fibonacci Numbers at Even indexes = " << sum <<endl;
+
+    return 0;
+}
