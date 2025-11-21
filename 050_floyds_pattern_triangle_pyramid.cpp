f5205fb824f592fb45f794df86767f9abcf563a6
Raihan Biswas <raihanbiswas2006@gmail.com>
2025-11-22 04:12:16 +0600

    Floyd's Pattern Triangle Pyramid

---
+++ 050_floyds_pattern_triangle_pyramid.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    
+    cout << "Give Number of Rows: ";
+    cin >> n;
+
+    int num = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+
+        for (int j = 1; j <= i; j++)
+        {
+            cout << num << " ";
+            num++;
+        }
+
+        cout <<endl;
+    }
+
+    return 0;
+}
