1d0a911fc25c3164396e01882ef0d887fc9140b6
Raihan Biswas <raihanbiswas2006@gmail.com>
2025-11-22 03:11:25 +0600

    Hollow Star Pyramid in a Diamond Shape

---
+++ 047_hollow_star_pyramid_in_a_diamond_shape.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n, i, j, s;
+
+    cout << "Give Number of Rows: ";
+    cin >> n;
+
+    for (i = 1; i <= n; i++)
+    {
+
+        for (s = 1; s <= n - i; s++)
+            cout << " ";
+
+        for (j = 1; j <= 2 * i - 1; j++)
+        {
+            if (j == 1 || j == 2 * i - 1)
+                cout << "*";
+            else
+                cout << " ";
+        }
+
+        cout <<endl;
+    }
+
+    for (i = n - 1; i >= 1; i--)
+    {
+
+        for (s = 1; s <= n - i; s++)
+            cout << " ";
+
+        for (j = 1; j <= 2 * i - 1; j++)
+        {
+            if (j == 1 || j == 2 * i - 1)
+                cout << "*";
+            else
+                cout << " ";
+        }
+
+        cout <<endl;
+    }
+
+    return 0;
+}
