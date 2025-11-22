b2da6ec9a92ef0ff5cc37056ac097869044ea6d0
Raihan Biswas <raihanbiswas2006@gmail.com>
2025-11-23 03:23:40 +0600

    Remove Duplicate Elements from a Sorted Array

---
+++ 072_remove_duplicates_sorted_array.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+
+    cout << "Enter Number of Elements: ";
+    cin >> n;
+
+    int arr[100];
+
+    cout << "Enter Sorted Array Elements:" <<endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    if (n == 0 || n == 1)
+    {
+        cout << "Array after Removing Duplicates: ";
+
+        for (int i = 0; i < n; i++)
+            cout << arr[i] << " ";
+        cout <<endl;
+        return 0;
+    }
+
+    int j = 0;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i] != arr[i + 1])
+        {
+            arr[j++] = arr[i];
+        }
+    }
+
+    arr[j++] = arr[n - 1];
+
+    cout << "Array after Removing Duplicates: ";
+
+    for (int i = 0; i < j; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout <<endl;
+
+    return 0;
+}
