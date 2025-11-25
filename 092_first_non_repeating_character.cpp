3564147f9edd1a8678281b18c5400167f1fdc8ba
Raihan Biswas <raihanbiswas2006@gmail.com>
2025-11-26 04:31:23 +0600

    Get a Non-Repeating Character from the Given String

---
+++ 092_first_non_repeating_character.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(){
+
+    char str[200];
+    int freq[256] = {0};
+
+    cout << "Give a String: ";
+    cin >> str;
+
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        freq[(unsigned char)str[i]]++;
+    }
+
+    char result = '\0';
+
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (freq[(unsigned char)str[i]] == 1)
+        {
+            result = str[i];
+            break;
+        }
+    }
+
+    if (result != '\0')
+        cout << "First Non-Repeating Character: " << result <<endl;
+    else
+        cout << "No Non-Repeating Character Found." <<endl;
+
+    return 0;
+}
