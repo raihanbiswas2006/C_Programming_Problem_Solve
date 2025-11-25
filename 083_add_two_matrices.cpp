c4cfa0869c232f3d69e84a6567eff288352cec29
Raihan Biswas <raihanbiswas2006@gmail.com>
2025-11-26 03:30:56 +0600

    Add Two Matrices

---
+++ 083_add_two_matrices.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows, cols;
+
+    cout << "Give Number of Rows: ";
+    cin >> rows;
+    cout << "Give Number of Columns: ";
+    cin >> cols;
+
+    int A[100][100], B[100][100], C[100][100];
+
+    cout << "Give Elements of Matrix A:" <<endl;
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cin >> A[i][j];
+        }
+    }
+
+    cout << "Give Elements of Matrix B:" <<endl;
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cin >> B[i][j];
+        }
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    cout << "Resultant Matrix (A + B): " <<endl;
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cout << C[i][j] << " ";
+        }
+        cout <<endl;
+    }
+
+    return 0;
+}
