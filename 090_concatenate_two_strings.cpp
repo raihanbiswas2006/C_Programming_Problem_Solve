d52a82b05e04dae42d43434bf3ea6c97f1ad46bf
Raihan Biswas <raihanbiswas2006@gmail.com>
2025-11-26 04:24:50 +0600

    Add or Concatenate Two Strings

---
+++ 090_concatenate_two_strings.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(){
+
+    char str1[100], str2[100];
+
+    cout << "Give First String: ";
+    cin >> str1;
+    cout << "Give Second String: ";
+    cin >> str2;
+
+    strcat(str1, str2);
+
+    cout << "Concatenated String = " << str1 <<endl;
+
+    return 0;
+}
